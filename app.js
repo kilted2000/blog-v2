711aea69fee7efb5f2d4ca36bd5f421b57ea3745
Richard <richardlwhittington@gmail.com>
2023-04-16 22:45:36 -0500

    add arrow

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ app.get("/posts/:postId", function(req, res){
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
+app.listen(PORT,()=> {
   console.log("Server started");
 });
